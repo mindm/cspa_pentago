a761f7f7f24cd7e2a888f15a418b231ebd49a19e
Arttu Hanska <arttu.hanska@gmail.com>
2013-03-09 21:27:22 +0200

    inital commit

---
+++ communication.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.2
+# -*- coding: iso-8859-15 -*-
+
+import inspect # http://docs.python.org/3/library/inspect.html
+import threading # http://docs.python.org/3/library/threading.html
+
+from model import GameLogic
+from interfaces import *
+from codecs import ClientPduCodec, ServerPduCodec
+
+class State: # default actions
+    
+    def name(self): 
+        return self.__class__.__name__
+
+    # add here methods with error-messages
+
+#class CommClient():
+#    entity = ClientPduCodec(self)
+ 
+
