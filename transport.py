8424c51cb588155f3af9f0035ee8d78373a3df19
mindm <maindi66@gmail.com>
2013-03-09 21:37:01 +0200

    Simple threaded echo server and client

--- transport.py
+++ transport.py
@@ -1,10 +1,55 @@
-#!/usr/bin/python
+#!/usr/bin/python3.2
 # -*- coding: iso-8859-15 -*-
 
 import socket
+import threading
 
-class Tcp:
+
+SIZE = 1024
+
+class TCPServer:
     def __init__(self, address, port):
         self.address = address
         self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.backlog = 5
+        
+        self.sock.bind((address, port))
+        self.sock.listen(self.backlog)
+        
+    # def accept(self):
+    #     self.client, self.address = self.sock.accept()
+
+    def loop(self):
+        while 1:
+            client = ClientThread(*self.sock.accept()).start()
+
+
+class TCPClient():
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, host):
+        self.sock.connect(host)
+
+    def send(self, data):
+        self.sock.send(data)
+
+    def close(self):
+        self.sock.close()
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, client, address):
+        self.client = client
+        self.address = address
+        super().__init__()
+
+    def run(self):
+        while 1:
+            data = self.client.recv(1024)
+            if data:
+                print(data)
 
