08d0d318ab9242cfb24b7a1906b79c8bba9bf257
Arttu Hanska <arttu.hanska@gmail.com>
2013-03-09 19:27:55 +0200

    fixed indents

--- codecs.py
+++ codecs.py
@@ -19,150 +19,150 @@ from interfaces import IGameCommClientPdu, IGameCommServerPdu
 
 class ClientPduCodec(IGameCommClientPdu):
 
-  def __init__(self,entity):
-    self.entity = entity
-    self.rbuf = bytes()# read buffer
+    def __init__(self,entity):
+        self.entity = entity
+        self.rbuf = bytes()# read buffer
   
-  def start_game_pdu(self):
-    msgtype = struct.pack("<I",0)
-    size = 2 + len(msgtype)
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype
+    def start_game_pdu(self):
+        msgtype = struct.pack("<I",0)
+        size = 2 + len(msgtype)
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype
   
-  def update_board_pdu(self, board_info):
-    msgtype = struct.pack("<I",1)
-    boarddata = bytes()
-    for x in range(0,6):
-      for y in range(0,6):
-        boarddata += board_info[x][y].encode("iso8859_15")
-    size = 2 + len(msgtype) + len(boarddata)   
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype + boarddata
+    def update_board_pdu(self, board_info):
+        msgtype = struct.pack("<I",1)
+        boarddata = bytes()
+        for x in range(0,6):
+          for y in range(0,6):
+              boarddata += board_info[x][y].encode("iso8859_15")
+        size = 2 + len(msgtype) + len(boarddata)   
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype + boarddata
   
-  def game_end_pdu(self, end_status):
-    msgtype = struct.pack("<I",2)
-    resultdata = struct.pack("<I",end_status)
-    size = 2 + len(msgtype) + len(resultdata)   
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype + resultdata
+    def game_end_pdu(self, end_status):
+        msgtype = struct.pack("<I",2)
+        resultdata = struct.pack("<I",end_status)
+        size = 2 + len(msgtype) + len(resultdata)   
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype + resultdata
 
-  def invalid_move_pdu(self):
-    msgtype = struct.pack("<I",3)
-    size = 2 + len(msgtype)   
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype
+    def invalid_move_pdu(self):
+        msgtype = struct.pack("<I",3)
+        size = 2 + len(msgtype)   
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype
 
-  def your_turn_pdu(self):
-    msgtype = struct.pack("<I",4)
-    size = 2 + len(msgtype)   
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype
+    def your_turn_pdu(self):
+        msgtype = struct.pack("<I",4)
+        size = 2 + len(msgtype)   
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype
   
-  def decode(self,data): # => decode binary data and call resolved message
-    ## handle read buffer
-    self.rbuf = self.rbuf + data
-    data = self.rbuf
-    ## decode header 
-    point = 0 
-    msgsize, = struct.unpack("<I", data[point:point+2]) # Remember tuple return
-    point += 2 
-    msgtype, = struct.unpack("<I", data[point:point+2])
-    point += 2
-    ## no we know message size, can remove it from read buffer
-    self.rbuf = self.rbuf[msgsize:] # rest data
+    def decode(self,data): # => decode binary data and call resolved message
+        ## handle read buffer
+        self.rbuf = self.rbuf + data
+        data = self.rbuf
+        ## decode header 
+        point = 0 
+        msgsize, = struct.unpack("<I", data[point:point+2]) # Remember tuple return
+        point += 2 
+        msgtype, = struct.unpack("<I", data[point:point+2])
+        point += 2
+        ## no we know message size, can remove it from read buffer
+        self.rbuf = self.rbuf[msgsize:] # rest data
 
-    ## 0 start_game_pdu
-    if msgtype == 0: 
-      self.entity.start_game_pdu()
+        ## 0 start_game_pdu
+        if msgtype == 0: 
+            self.entity.start_game_pdu()
       
-    ## 1 update_board_pdu
-    elif msgtype == 1: 
-      board_info = list()
-      for x in range(0,6):
-        board_info.append(list())
-        for y in range(0,6):
-          board_info[x].append(None)
-      for i,byte in enumerate(data[point:point+36]):
-        y = i % 6
-        x = int(i / 6)
-        board_info[x][y] = chr(byte)
-      self.entity.update_board_pdu(board_info)
+        ## 1 update_board_pdu
+        elif msgtype == 1: 
+            board_info = list()
+            for x in range(0,6):
+                board_info.append(list())
+                for y in range(0,6):
+                    board_info[x].append(None)
+            for i,byte in enumerate(data[point:point+36]):
+                y = i % 6
+                x = int(i / 6)
+                board_info[x][y] = chr(byte)
+            self.entity.update_board_pdu(board_info)
 
-    ## 2 game_end_pdu(self, end_status):
-    elif msgtype == 2:
-      end_status, = struct.unpack("<I", data[point:point+2])
-      point += 2
-      self.entity.game_end_pdu(end_status)
+        ## 2 game_end_pdu(self, end_status):
+        elif msgtype == 2:
+            end_status, = struct.unpack("<I", data[point:point+2])
+            point += 2
+            self.entity.game_end_pdu(end_status)
       
-    ## 3 invalid_move_pdu(self):
-    elif msgtype == 3:
-      self.entity.invalid_move_pdu()
+        ## 3 invalid_move_pdu(self):
+        elif msgtype == 3:
+            self.entity.invalid_move_pdu()
 
-    ## 4 your_turn_pdu(self):
-    elif msgtype == 4:
-      self.entity.your_turn_pdu()
+        ## 4 your_turn_pdu(self):
+        elif msgtype == 4:
+            self.entity.your_turn_pdu()
 
-    ## X unknown
-    else:
-      raise RuntimeError("Bad message coding {}".format(msgtype))
+        ## X unknown
+        else:
+            raise RuntimeError("Bad message coding {}".format(msgtype))
 
 class ServerPduCodec(IGameCommServerPdu):
   
-  def __init__(self,entity):
-    self.entity = entity  
+    def __init__(self,entity):
+        self.entity = entity  
   
-  def look_game_pdu(self, game_id):
-    msgtype = struct.pack("<I",5)
-    gamedata = resultdata = struct.pack("<I",game_id)
-    size = 2 + len(msgtype) + len(strsize) + len(gamedata)
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype + gamedata
+    def look_game_pdu(self, game_id):
+        msgtype = struct.pack("<I",5)
+        gamedata = resultdata = struct.pack("<I",game_id)
+        size = 2 + len(msgtype) + len(strsize) + len(gamedata)
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype + gamedata
  
-  def m_place_pdu(self, x, y):
-    msgtype = struct.pack("<I",6)
-    xdata = struct.pack("<I",x)
-    ydata = struct.pack("<I",y)
-    size = 2 + len(msgtype) + len(xdata) + len(ydata)
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype + xdata + ydata
+    def m_place_pdu(self, x, y):
+        msgtype = struct.pack("<I",6)
+        xdata = struct.pack("<I",x)
+        ydata = struct.pack("<I",y)
+        size = 2 + len(msgtype) + len(xdata) + len(ydata)
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype + xdata + ydata
 
-  def rotate_board_pdu(self, board, direction):
-    msgtype = struct.pack("<I",7)
-    subboarddata = struct.pack("<I",board)
-    directiondata = struct.pack("<H",direction)
-    size = 2 + len(msgtype) + len(subboarddata) + len(directiondata)
-    msgsize = struct.pack("<I",size)
-    return msgsize + msgtype + subboarddata + directiondata
+    def rotate_board_pdu(self, board, direction):
+        msgtype = struct.pack("<I",7)
+        subboarddata = struct.pack("<I",board)
+        directiondata = struct.pack("<H",direction)
+        size = 2 + len(msgtype) + len(subboarddata) + len(directiondata)
+        msgsize = struct.pack("<I",size)
+        return msgsize + msgtype + subboarddata + directiondata
 
-  def decode(self, cid, data): # => decode binary data and call resolved message
-    point = 0 
-    msgsize, = struct.unpack("<I", data[point:point+2]) # Remember tuple return
-    point += 2 
-    msgtype, = struct.unpack("<I", data[point:point+2])
-    point += 2
+    def decode(self, cid, data): # => decode binary data and call resolved message
+        point = 0 
+        msgsize, = struct.unpack("<I", data[point:point+2]) # Remember tuple return
+        point += 2 
+        msgtype, = struct.unpack("<I", data[point:point+2])
+        point += 2
     
-    # look_game_pdu(self, game_id):
-    if msgtype == 5:
-      game_id, = struct.unpack("<I", data[point:point+2])
-      point += 2
-      self.entity.look_game_pdu(game_id)
-    
-    # m_place_pdu(self, x, y):
-    elif msgtype == 6:
-      x, = struct.unpack("<I", data[point:point+2])
-      point += 2
-      y, = struct.unpack("<I", data[point:point+2])
-      point += 2
-      self.entity.m_place_pdu(x, y)
+        # look_game_pdu(self, game_id):
+        if msgtype == 5:
+            game_id, = struct.unpack("<I", data[point:point+2])
+            point += 2
+            self.entity.look_game_pdu(game_id)
+        
+        # m_place_pdu(self, x, y):
+        elif msgtype == 6:
+            x, = struct.unpack("<I", data[point:point+2])
+            point += 2
+            y, = struct.unpack("<I", data[point:point+2])
+            point += 2
+            self.entity.m_place_pdu(x, y)
 
-    # rotate_board_pdu(self, board, direction):
-    elif msgtype == 7:
-      board, = struct.unpack("<I", data[point:point+2])
-      point += 2
-      direction, = struct.unpack("<H", data[point:point+2])
-      point += 2
-      self.entity.rotate_board_pdu(board, direction)
+        # rotate_board_pdu(self, board, direction):
+        elif msgtype == 7:
+            board, = struct.unpack("<I", data[point:point+2])
+            point += 2
+            direction, = struct.unpack("<H", data[point:point+2])
+            point += 2
+            self.entity.rotate_board_pdu(board, direction)
 
-    ## X unknown
-    else:
-      raise RuntimeError("Bad message coding {}".format(msgtype))
+        ## X unknown
+        else:
+            raise RuntimeError("Bad message coding {}".format(msgtype))
--- interfaces.py
+++ interfaces.py
@@ -7,190 +7,190 @@ from abc import ABCMeta, abstractmethod # http://docs.python.org/3/library/abc.h
 
 class IGameCommClientReq(metaclass=ABCMeta):
 
-  @abstractmethod
-  def look_game_req(self, game_id):
-    """ Player is looking for game.
-    game_id as uint32 
-    """
+    @abstractmethod
+    def look_game_req(self, game_id):
+        """ Player is looking for game.
+        game_id as uint32 
+        """
       
-  @abstractmethod
-  def m_place_req(self, x, y):
-    """ Player places a game piece.
-    x as uint32 
-    y as uint32
-    """
+    @abstractmethod
+    def m_place_req(self, x, y):
+        """ Player places a game piece.
+        x as uint32 
+        y as uint32
+        """
 
-  @abstractmethod
-  def rotate_board_req(self, board, direction):
-    """ Player rotates sub-board
-    board as uint32
-    direction as uint8 
-    """
+    @abstractmethod
+    def rotate_board_req(self, board, direction):
+        """ Player rotates sub-board
+        board as uint32
+        direction as uint8 
+        """
         
 class IGameCommClientInd(metaclass=ABCMeta):
   
-  @abstractmethod
-  def start_game_ind(self):
-    """ Player is informed of starting game.
-    """
+    @abstractmethod
+    def start_game_ind(self):
+        """ Player is informed of starting game.
+        """
 
-  @abstractmethod
-  def update_board_ind(self, board_info):
-    """ Board update from server.
-    board_info as list  
-    """
+    @abstractmethod
+    def update_board_ind(self, board_info):
+        """ Board update from server.
+        board_info as list  
+        """
 
-  @abstractmethod
-  def game_end_ind(self, end_status):
-    """ Game ends; win, lose or draw.
-    end_status as uint32
-    """       
+    @abstractmethod
+    def game_end_ind(self, end_status):
+        """ Game ends; win, lose or draw.
+        end_status as uint32
+        """       
 
-  @abstractmethod
-  def invalid_move_ind(self):
-    """ Game logic error.
-    """
+    @abstractmethod
+    def invalid_move_ind(self):
+        """ Game logic error.
+        """
 
-  @abstractmethod
-  def your_turn_ind(self):
-    """ Player's turn now. 
-    """
+    @abstractmethod
+    def your_turn_ind(self):
+        """ Player's turn now. 
+        """
         
 class IGameCommClientPdu(metaclass=ABCMeta):
 
-  @abstractmethod
-  def start_game_pdu(self):
-    """ PDU for player looking a new game.
-    """
+    @abstractmethod
+    def start_game_pdu(self):
+        """ PDU for player looking a new game.
+        """
 
-  @abstractmethod
-  def update_board_pdu(self, board_info):
-    """ PDU for player move.
-    board_info as list
-    """
+    @abstractmethod
+    def update_board_pdu(self, board_info):
+        """ PDU for player move.
+        board_info as list
+        """
 
-  @abstractmethod
-  def game_end_pdu(self, end_status):
-    """ PDU for end game information.
-    end_status as uint32
-    """
+    @abstractmethod
+    def game_end_pdu(self, end_status):
+        """ PDU for end game information.
+        end_status as uint32
+        """
 
-  @abstractmethod
-  def invalid_move_pdu(self):
-    """ PDU for move againsta game logic.
-    """
+    @abstractmethod
+    def invalid_move_pdu(self):
+        """ PDU for move againsta game logic.
+        """
 
-  @abstractmethod
-  def your_turn_pdu(self):
-    """ PDU for players turn.
-    """
+    @abstractmethod
+    def your_turn_pdu(self):
+        """ PDU for players turn.
+        """
 
 # Server interfaces     
 
 class IGameCommServerReq(metaclass=ABCMeta):
 
-  @abstractmethod
-  def game_end_req(self, end_status):
-    """ End condition is met  
-    end_status as uint32
-    """
+    @abstractmethod
+    def game_end_req(self, end_status):
+        """ End condition is met  
+        end_status as uint32
+        """
 
-  @abstractmethod
-  def update_board_req(self, board_info):
-    """ Game logic tells the server to send new board to client.
-    board_info as list
-    """
+    @abstractmethod
+    def update_board_req(self, board_info):
+        """ Game logic tells the server to send new board to client.
+        board_info as list
+        """
 
-  @abstractmethod
-  def invalid_move_req(self):
-    """ Game logic tells the server that the move was invalid  
-    """
+    @abstractmethod
+    def invalid_move_req(self):
+        """ Game logic tells the server that the move was invalid  
+        """
 
 class IGameCommServerInd(metaclass=ABCMeta):
 
-  @abstractmethod
-  def new_game_ind(self):
-    """ Server asks game logic for a new game
-    """
+    @abstractmethod
+    def new_game_ind(self):
+        """ Server asks game logic for a new game
+        """
     
-  @abstractmethod
-  def m_place_ind(self, x, y):
-    """ Server tells game logic about new game piece placement.  
-    x as uint32
-    y as uint32
-    """
+    @abstractmethod
+    def m_place_ind(self, x, y):
+        """ Server tells game logic about new game piece placement.  
+        x as uint32
+        y as uint32
+        """
 
-  @abstractmethod
-  def rotate_board_ind(self, board, direction):
-    """ Server tells game logic about sub-board rotation.
-    board as uint32
-    direction as uint8
-    """
+    @abstractmethod
+    def rotate_board_ind(self, board, direction):
+        """ Server tells game logic about sub-board rotation.
+        board as uint32
+        direction as uint8
+        """
 
 class IGameCommServerPdu(metaclass=ABCMeta):
   
- @abstractmethod
-  def look_game_pdu(self, game_id):
-    """ Client looking for game.
-    game_id as uint32
-    """
+    @abstractmethod
+    def look_game_pdu(self, game_id):
+        """ Client looking for game.
+        game_id as uint32
+        """
 
-  @abstractmethod
-  def m_place_pdu(self, x, y):
-    """ Client sends placement data.
-    x as uint32 
-    y as uint32 
-    """
+    @abstractmethod
+    def m_place_pdu(self, x, y):
+        """ Client sends placement data.
+        x as uint32 
+        y as uint32 
+        """
 
-  @abstractmethod
-  def rotate_board_pdu(self, board, direction):
-    """ Client sends rotation data.
-    board as uint32
-    direction as uint8
-    """
+    @abstractmethod
+    def rotate_board_pdu(self, board, direction):
+        """ Client sends rotation data.
+        board as uint32
+        direction as uint8
+        """
 
 # Transport, Tcp Interfaces    
     
 class ITransReq(metaclass=ABCMeta):
   
-  @abstractmethod
-  def send(self):
-    """ Send data to peer.
-    """
+    @abstractmethod
+    def send(self):
+        """ Send data to peer.
+        """
     
-  @abstractmethod
-  def open_connection(self):
-    """ Open connection.
-    """
+    @abstractmethod
+    def open_connection(self):
+        """ Open connection.
+        """
 
-  @abstractmethod
-  def close_connection(self):
-    """ Close connection.
-    """
+    @abstractmethod
+    def close_connection(self):
+        """ Close connection.
+        """
     
 class ITransInd(metaclass=ABCMeta):
 
-  @abstractmethod
-  def receive(self):
-    """ Receive data from connection
-    """
+    @abstractmethod
+    def receive(self):
+        """ Receive data from connection
+        """
 
-  @abstractmethod
-  def new_connection_ind(self):
-    """ New connection 
-    """
+    @abstractmethod
+    def new_connection_ind(self):
+        """ New connection 
+        """
     
-  @abstractmethod
-  def close_connection_ind(self):
-    """ Connection has closed.
-    """
+    @abstractmethod
+    def close_connection_ind(self):
+        """ Connection has closed.
+        """
 
-  @abstractmethod
-  def network_error(self):
-    """ Error in network.
-    """
+    @abstractmethod
+    def network_error(self):
+        """ Error in network.
+        """
 
-  @abstractmethod
-  def transport_error(self):
-    """ Error in transport.
-    """
+    @abstractmethod
+    def transport_error(self):
+        """ Error in transport.
+        """
